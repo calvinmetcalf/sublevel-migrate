b7e6f21b8ab7365ed83b5c1abaca7351002b8aac
Dominic Tarr <dominic.tarr@gmail.com>
2014-08-25 12:54:44 -0500

    use level-write-stream, since internal write streams is to be removed

--- index.js
+++ index.js
@@ -1,34 +1,45 @@
 var through = require('through2').obj;
-var bytewise = require('level-sublevel/codec');
+var legacy = require('level-sublevel/codec/legacy');
+var LevelWriteStream = require('level-write-stream');
 
 function identity (item){
   return item;
 }
 
-module.exports = function (db, outDB, callback) {
-  if (typeof callback === 'undefined') {
-    callback = outDB;
-    outDB = db;
-  }
-  db.createReadStream()
-    .on('error', callback)
-    .pipe(through(function (item, _, next) {
-      this.push({
-        type:'del',
-        key:item.key
+module.exports = create(require('level-sublevel/codec/index'))
+module.exports.bytewise = create(require('level-sublevel/codec/bytewise'))
+
+function create (codec) {
+  return function (db, outDB, callback) {
+    if (typeof callback === 'undefined') {
+      callback = opts;
+      opts = null;
+    }
+    if (typeof callback === 'undefined') {
+      callback = outDB;
+      outDB = db;
+    }
+    db.createReadStream()
+      .on('error', callback)
+      .pipe(through(function (item, _, next) {
+        if(db === outDB)
+          this.push({
+            type:'del',
+            key:item.key
+          });
+        var key = legacy.decode(item.key)
+
+        this.push({
+          key: codec.encode(key),
+          value: item.value
+        });
+        next();
+      }))
+      .on('error', callback)
+      .pipe(LevelWriteStream(outDB)())
+      .on('error', callback)
+      .on('finish', function () {
+        callback();
       });
-      var split = item.key.split('\xff').filter(identity);
-      var key = split.pop();
-      this.push({
-        key: bytewise.encode([split, key]),
-        value: item.value
-      });
-      next();
-    }))
-    .on('error', callback)
-    .pipe(outDB.createWriteStream())
-    .on('error', callback)
-    .on('finish', function () {
-      callback();
-    });
+  };
 };
